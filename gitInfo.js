4ac4672f1566ddc6a295de238a74f916a694ffd7
Isaac Boussum <isaacboussum10@gmail.com>
2023-01-22 19:28:23 -0700

    git info complete

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,8 @@
 
 //CODE HERE
 
+let gitDefinition = "a git tracks changes in a file or set of files. They are good when multiple programmers are working on projects because they can see changes and when the changes happened"
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition = "Git hub is a website when you can submit your code you are working on. You can make a repository and upload it to get to show others what you are working on and what changes you have made along the way"
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -27,6 +31,8 @@
 
 //CODE HERE
 
+let gitInitDefinition = "git init creates a new git repository. you have to do this first in order to upload it to github. Once it is initialized the first time you dont need to worry about doing it again. "
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -35,6 +41,8 @@
 
 //CODE HERE
 
+let gitCloneDefinition = "git clone is used to point to an existing repo and to make a copy of it."
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +50,8 @@
 */
 
 //CODE HERE
+
+let gitStatusDefinition = "git status lets you know when changes have been made to certain files in your git. So if you have 3 files in a folder and you modify one, it will show you the one that has changes to it."
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +64,9 @@
 
 //CODE HERE
 
+let gitAddDefinition = "you do git add to add the files you want to push to your repository"
+
+let gitAddCode = "add ."
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +78,10 @@
 
 //CODE HERE
 
+let git gitCommitDefinition = "git commit allows you to making comments about progress along the way. it is good to do this and to be specific because toher coders can see when and why changes where made. It makes it more organized"
+
+let gitCommitCode = git commit -m "initial commit"
+
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +90,5 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = "after you add and comment, you must push it to your existing reposition"
